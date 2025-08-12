237a496ceba1654635c69b6073ba0536655c30e9
Angie Sanchez Gallego <angie@MacBook-Pro-de-Angie.local>
2025-08-12 19:46:01 +0200

    bug eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,22 @@ const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
 
-    // Copiar archivos estáticos (CSS, JS, imágenes) a la carpeta de salida
-    eleventyConfig.addPassthroughCopy("./src/dist");
+    // Copiar archivos estáticos (imágenes y scripts) a la carpeta de salida.
+    // Hemos eliminado la línea que copiaba la carpeta 'dist'.
     eleventyConfig.addPassthroughCopy("./src/images");
     eleventyConfig.addPassthroughCopy("./src/scripts.js");
+    // También copiaremos los archivos de la raíz como robots.txt y sitemap.xml
+    eleventyConfig.addPassthroughCopy("./src/robots.txt");
+    eleventyConfig.addPassthroughCopy("./src/sitemap.xml");
+
 
     // Formateador de fechas legible
     eleventyConfig.addFilter("readableDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).setLocale('es').toLocaleString(DateTime.DATE_FULL);
+        // Corregido para manejar correctamente las fechas desde el frontmatter
+        if (dateObj) {
+            return DateTime.fromJSDate(dateObj, {zone: 'utc'}).setLocale('es').toLocaleString(DateTime.DATE_FULL);
+        }
+        return '';
     });
 
     return {
@@ -19,5 +27,4 @@ module.exports = function(eleventyConfig) {
         }
     };
 };
-
 
