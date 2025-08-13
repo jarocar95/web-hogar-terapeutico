7f716a5888cb1b3ee0fb5769208ebde9966acdc8
Angie Sanchez Gallego <angie@MacBook-Pro-de-Angie.local>
2025-08-13 23:02:51 +0200

    Nuevo plugin 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,34 @@
 const { DateTime } = require("luxon");
+const Image = require("@11ty/eleventy-img");
+
+// Función asíncrona para el shortcode de imágenes
+async function imageShortcode(src, alt, sizes = "100vw") {
+    // Opciones para el procesamiento de imágenes
+    let metadata = await Image(src, {
+	widths: [300, 600, 900, "auto"],
+	formats: ["webp", "jpeg"],
+	// La carpeta de salida debe coincidir con la de tu configuración
+	outputDir: "./public/img/",
+	// La URL base para el atributo src
+	urlPath: "/img/",
+    });
+    
+    // Atributos para la etique <img>
+    let imageAttributes = {
+	alt,
+	sizes,
+	loading: "lazy",
+	decoding: "async",
+    };
+
+    // Genera el HTML completo del elemento <picture>
+    return Image.generateHTML(metadata, imageAttributes);
+}
 
 module.exports = function(eleventyConfig) {
 
+    eleventyConfig.addAsyncShortcode("image", imageShortcode);
     eleventyConfig.addPassthroughCopy("./src/dist");
-    // Copiar archivos estáticos (imágenes y scripts) a la carpeta de salida.
-    // Hemos eliminado la línea que copiaba la carpeta 'dist'.
-    eleventyConfig.addPassthroughCopy("./src/images");
     eleventyConfig.addPassthroughCopy("./src/scripts.js");
     // También copiaremos los archivos de la raíz como robots.txt y sitemap.xml
     eleventyConfig.addPassthroughCopy("./src/robots.txt");
